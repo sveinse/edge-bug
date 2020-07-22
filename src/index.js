72f9e3a38304f065b2a39600e184d9df2d67f157
Svein Seldal <sveinse@seldal.com>
2020-07-23 01:00:38 +0200

    Added index.js without semis

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import semver from "semver"
+
+const find_latest = relinfo => {
+  let latest = {}
+  for (let vinfo of relinfo) {
+    let sver = vinfo.version.replace("-b", ".0-beta.")
+    if (
+      !latest[vinfo.train] ||
+      semver.gt(sver, latest[vinfo.train].semver, { includePrerelease: true })
+    ) {
+      latest[vinfo.train] = {
+        ...vinfo,
+        semver: sver,
+      }
+    }
+  }
+  return latest
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
